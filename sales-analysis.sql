d0228a8a9091116fa6c991fde71e7f732a1740ff
Mr. Mishra <46827128+MrMishra123@users.noreply.github.com>
2026-03-02 13:16:43 +0530

    Create sales-analysis.sql

---
+++ sales-analysis.sql
@@ -0,0 +1,36 @@
+-- ==========================================
+-- PROJECT: SALES ANALYSIS
+-- Dataset: Superstore.csv
+-- Objective: Analyze revenue, profit, and sales trends
+-- ==========================================
+
+-- 1. Total Revenue
+SELECT SUM(Sales) AS total_revenue
+FROM superstore;
+
+-- 2. Top Products
+SELECT `Product Name`,
+       SUM(Sales) AS revenue
+FROM superstore
+GROUP BY `Product Name`
+ORDER BY revenue DESC
+LIMIT 10;
+
+-- 3. Sales by Region
+SELECT Region,
+       SUM(Sales) AS regional_sales
+FROM superstore
+GROUP BY Region
+ORDER BY regional_sales DESC;
+
+-- 4. Monthly Sales Trend (MySQL)
+SELECT DATE_FORMAT(`Order Date`, '%Y-%m') AS month,
+       SUM(Sales) AS monthly_sales
+FROM superstore
+GROUP BY month
+ORDER BY month;
+
+-- 5. Loss-making Orders
+SELECT *
+FROM superstore
+WHERE Profit < 0;
